e2c24de12b3a19fae9f73566bd905ac20d5ff394
hi-rola <rolaalbaa@gmail.com>
2020-08-04 13:17:56 -0500

    Se agregan distintas rutas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,13 @@ import { PrincipalAcademicoComponent } from './components/academico/principal-ac
 import { GuardiaAcademicoService } from './services/guardias/guardia-academico.service';
 import { EditarEstudianteComponent } from './components/academico/editar-estudiante/editar-estudiante.component';
 import { RegistrarEstudianteComponent } from './components/academico/registrar-estudiante/registrar-estudiante.component';
+import { RegistrarAcademicoComponent } from './components/administrador/registrar-academico/registrar-academico.component';
+import { PrincipalAdministrador2Component } from './components/administrador/principal-administrador2/principal-administrador2.component';
+import { EditarAcademicoComponent } from './components/administrador/editar-academico/editar-academico.component';
+import { PrincipalAdministrador1Component } from './components/administrador/principal-administrador1/principal-administrador1.component';
+import { AcercaDeComponent } from './components/encabezados/menu/acerca-de/acerca-de.component';
+import { Error401Component } from './components/errores/error401/error401.component';
+import { Error404Component } from './components/errores/error404/error404.component';
 
 
 
@@ -14,6 +21,13 @@ const routes: Routes = [
   { path: 'home-academico', component: PrincipalAcademicoComponent, canActivate: [GuardiaAcademicoService]},
   { path: 'home-academico/actualizar-inf-estudiante/:id', component: EditarEstudianteComponent, canActivate: [GuardiaAcademicoService]},
   { path: 'registrar-estudiante', component: RegistrarEstudianteComponent},
+  { path: 'registrar-academico', component: RegistrarAcademicoComponent },
+  { path: 'academicos-registrados', component: PrincipalAdministrador2Component },
+  { path: 'actualizar-informacion-academico/:id', component: EditarAcademicoComponent },
+  { path: 'home-administrador', component: PrincipalAdministrador1Component },
+  { path: 'acerca-de', component: AcercaDeComponent },
+  { path: '401', component: Error401Component },
+  { path: '**', component: Error404Component}
 ];
 
 @NgModule({
