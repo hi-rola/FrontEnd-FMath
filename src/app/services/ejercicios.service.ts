d293639fc05efe309dd71abb49d12deb2d00f748
hi-rola <rolaalbaa@gmail.com>
2020-08-04 00:25:43 -0500

    Servicio ejercicio y puntaje estudiante finalizado.

--- src/app/services/ejercicios.service.ts
+++ src/app/services/ejercicios.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EjerciciosService {
 
-  constructor() { }
+  urlBackEnd: string;
+
+  constructor(private http: HttpClient) { this.urlBackEnd = environment.backEndURL + '/ejercicio'; }
+
+  getTodosEjerciciosUnion(): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/ejercicios/');
+  }
+
+  getTodosEjerciciosInterseccion(): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/ejercicios/interseccion/');
+  }
+
+  getTodosEjerciciosDiferencia(): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/ejercicios/diferencia/');
+  }
+
+  getEjerciciosPorTipo(tipo: String): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/ejercicios/' + tipo);
+  }
+
+  getEjerciciosPorTipoNivel1(tipo: String): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/ejerciciosNivel1/' + tipo);
+  }
+
+  getEjerciciosPorTipoNivel2(tipo: String): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/ejerciciosNivel2/' + tipo);
+  }
 }
--- src/app/services/puntajeestudiante.service.ts
+++ src/app/services/puntajeestudiante.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { PuntajeEjercicio } from '../models/PuntajeEjercicio';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PuntajeestudianteService {
 
-  constructor() { }
+  urlBackEnd: string;
+
+  constructor(private http: HttpClient) { this.urlBackEnd = environment.backEndURL + '/puntaje'; }
+
+  getPuntaje(matricula: string): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/puntajes/' + matricula);
+  }
+
+  resgistrarPuntaje(puntajeEstudiante: PuntajeEjercicio): Observable<any> {
+    return this.http.post(this.urlBackEnd + '/puntajes/', puntajeEstudiante);
+  }
 }
