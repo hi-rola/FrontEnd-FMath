654d31a0373ca93738c18699e300b31c3ef853c3
hi-rola <rolaalbaa@gmail.com>
2020-08-04 00:06:34 -0500

    Diseño mensaje academico registrado finalizado.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,14 +13,36 @@ import { MaterialDesignModule } from './models/MaterialDesign.module';
 import { InterceptorhttpService } from './services/interceptorhttp.service';
 import { LoginComponent } from './components/login/login.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MsjAcademicoRegistradoComponent } from './components/mensajes-de-confirmacion/msj-academico-registrado/msj-academico-registrado.component';
+import { MsjActualizarDatosComponent } from './components/mensajes-de-confirmacion/msj-actualizar-datos/msj-actualizar-datos.component';
+import { MsjCambiarEstatusComponent } from './components/mensajes-de-confirmacion/msj-cambiar-estatus/msj-cambiar-estatus.component';
+import { MsjCancelarRegistroComponent } from './components/mensajes-de-confirmacion/msj-cancelar-registro/msj-cancelar-registro.component';
+import { MsjEliminarEstudianteComponent } from './components/mensajes-de-confirmacion/msj-eliminar-estudiante/msj-eliminar-estudiante.component';
+import { MsjEstudianteActualizadoComponent } from './components/mensajes-de-confirmacion/msj-estudiante-actualizado/msj-estudiante-actualizado.component';
+import { MsjEstudianteEliminadoComponent } from './components/mensajes-de-confirmacion/msj-estudiante-eliminado/msj-estudiante-eliminado.component';
+import { MsjEstudianteExisteComponent } from './components/mensajes-de-confirmacion/msj-estudiante-existe/msj-estudiante-existe.component';
+import { MsjEstudianteRegistradoComponent } from './components/mensajes-de-confirmacion/msj-estudiante-registrado/msj-estudiante-registrado.component';
+import { MsjExisteAcademicoComponent } from './components/mensajes-de-confirmacion/msj-existe-academico/msj-existe-academico.component';
+import { MsjRegistrarAcademicoComponent } from './components/mensajes-de-confirmacion/msj-registrar-academico/msj-registrar-academico.component';
+import { MsjResgistrarEstudianteComponent } from './components/mensajes-de-confirmacion/msj-resgistrar-estudiante/msj-resgistrar-estudiante.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent, LoginComponent, FooterComponent
+    AppComponent, LoginComponent, FooterComponent, MsjAcademicoRegistradoComponent, MsjActualizarDatosComponent,
+    MsjCambiarEstatusComponent, MsjCancelarRegistroComponent, MsjEliminarEstudianteComponent, MsjEstudianteActualizadoComponent,
+    MsjEstudianteEliminadoComponent, MsjEstudianteExisteComponent, MsjEstudianteRegistradoComponent, MsjExisteAcademicoComponent, 
+    MsjRegistrarAcademicoComponent, MsjResgistrarEstudianteComponent,
   ],
   imports: [
-    BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialDesignModule, HttpClientModule, 
+    BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialDesignModule, HttpClientModule,
     ReactiveFormsModule, FormsModule, NgxSpinnerModule, FlexLayoutModule
+  ],
+  entryComponents: [
+    MsjAcademicoRegistradoComponent, MsjActualizarDatosComponent, MsjCambiarEstatusComponent, MsjCancelarRegistroComponent,
+    MsjEliminarEstudianteComponent, MsjEstudianteActualizadoComponent, MsjEstudianteEliminadoComponent, MsjEstudianteExisteComponent,
+    MsjEstudianteRegistradoComponent, MsjExisteAcademicoComponent, MsjRegistrarAcademicoComponent, MsjResgistrarEstudianteComponent
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: InterceptorhttpService, multi: true }
