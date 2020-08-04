60ddd2036dc7de392fc1abadfd630ddf448f8177
hi-rola <rolaalbaa@gmail.com>
2020-08-04 13:57:19 -0500

    Diseño principal estudiante finalizado.

---
+++ src/app/components/estudiante/principal-estudiante/principal-estudiante.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-estudiante',
+  templateUrl: './principal-estudiante.component.html',
+  styleUrls: ['./principal-estudiante.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class PrincipalEstudianteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
