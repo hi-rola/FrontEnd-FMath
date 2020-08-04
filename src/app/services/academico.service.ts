5b76096ae41ef1f759eca5a8ca498a236e4b4fdd
hi-rola <rolaalbaa@gmail.com>
2020-08-04 00:18:00 -0500

    Verificar MÉTODO  linea 12.

--- src/app/services/academico.service.ts
+++ src/app/services/academico.service.ts
@@ -1,9 +1,36 @@
+import { EstudianteNumeropersonal } from './../models/EstudianteNumeropersonal';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Estudiante } from './../models/Estudiante';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AcademicoService {
+  getTodosEstudiantesPor() {
+    throw new Error("Method not implemented.");
+  } 
+  ///verificar
 
-  constructor() { }
+  urlBackEnd: string;
+
+  constructor(private http: HttpClient) { this.urlBackEnd = environment.backEndURL; }
+
+  getTodosEstudiantes(): Observable<any> {
+    return this.http.get(this.urlBackEnd + '/estudiantes');
+  }
+
+  nuevoEstudiante(estudianteNumeroPersonal: EstudianteNumeropersonal): Observable<any> {
+    return this.http.post(this.urlBackEnd + '/estudiantes/', estudianteNumeroPersonal);
+  }
+
+  actualizarEstudiante(estudiante: Estudiante): Observable<any> {
+    return this.http.put(this.urlBackEnd + '/estudiantes/' + estudiante.matricula, estudiante);
+  }
+
+  deleteEstudiante(estudiante: Estudiante): Observable<any> {
+    return this.http.delete(this.urlBackEnd + '/estudiantes/' + estudiante.matricula);
+  }
 }
