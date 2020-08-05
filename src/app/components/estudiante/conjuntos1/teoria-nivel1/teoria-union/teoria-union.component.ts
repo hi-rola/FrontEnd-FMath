c7a7c65d776dba4b11873a341f2c4c41a7a873fd
hi-rola <rolaalbaa@gmail.com>
2020-08-04 23:34:41 -0500

    Teoría unión finalizada.

--- src/app/components/estudiante/conjuntos1/teoria-nivel1/teoria-union/teoria-union.component.ts
+++ src/app/components/estudiante/conjuntos1/teoria-nivel1/teoria-union/teoria-union.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-teoria-union',
   templateUrl: './teoria-union.component.html',
-  styleUrls: ['./teoria-union.component.scss']
+  styleUrls: ['./teoria-union.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TeoriaUnionComponent implements OnInit {
 
