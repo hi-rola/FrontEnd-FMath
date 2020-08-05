ab03924f7ce16264d08e19dc9c0822230c26324f
hi-rola <rolaalbaa@gmail.com>
2020-08-04 22:15:05 -0500

    Se agrega model para Ejercicio nivel 1 y 2.

---
+++ src/app/models/Ejercicionivel1.ts
@@ -0,0 +1,19 @@
+export class Ejercicionivel1{
+    constructor(
+        public idejercicionivel1: number,
+        public numeropregunta: string,
+        public pregunta: string,
+        public conjuntoa: string,
+        public conjuntob: string,
+        public respuestaa: string,
+        public respuestab: string,
+        public respuestac: string,
+        public respuestad: string,
+        public respuestacorrecta: string,
+        public solucionrespuestaa: string,
+        public solucionrespuestab: string,
+        public solucionrespuestac: string,
+        public solucionrespuestad: string,
+        public tipo: string
+    ) { }
+}
---
+++ src/app/models/Ejercicionivel2.ts
@@ -0,0 +1,20 @@
+export class Ejercicionivel2{
+    constructor(
+        public idejercicionivel2: number,
+        public numeropregunta: string,
+        public pregunta: string,
+        public conjuntoa: string,
+        public conjuntob: string,
+        public conjuntoc: string,
+        public respuestaa: string,
+        public respuestab: string,
+        public respuestac: string,
+        public respuestad: string,
+        public respuestacorrecta: string,
+        public solucionrespuestaa: string,
+        public solucionrespuestab: string,
+        public solucionrespuestac: string,
+        public solucionrespuestad: string,
+        public tipo: string
+    ) { }
+}
