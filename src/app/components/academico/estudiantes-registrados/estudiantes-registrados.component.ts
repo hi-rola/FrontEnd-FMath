00badb43f67e88a3fbf02e1736ccee941b4f80e6
hi-rola <rolaalbaa@gmail.com>
2020-08-09 13:29:18 -0500

    Se agrega diseño responsivo.

--- src/app/components/academico/estudiantes-registrados/estudiantes-registrados.component.ts
+++ src/app/components/academico/estudiantes-registrados/estudiantes-registrados.component.ts
@@ -27,7 +27,7 @@ export class EstudiantesRegistradosComponent implements OnInit {
   ocultarMensaje = true;
 
 
-  displayedColumns: string[] = ['matricula', 'nombrecompleto', 'programaeducativo', 'seccion', 'bloque', 'actions'];
+  displayedColumns: string[] = ['matricula', 'nombrecompleto', 'programaeducativo', 'seccion', 'actions'];
   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: true }) sort: MatSort;
 
@@ -51,6 +51,11 @@ export class EstudiantesRegistradosComponent implements OnInit {
           this.dataSource = new MatTableDataSource<Estudiante>(this.estudiantes);
           this.dataSource.paginator = this.paginator;
           this.dataSource.sort = this.sort;
+          this.dataSource.paginator._intl.itemsPerPageLabel = "Usuarios por página";
+          this.dataSource.paginator._intl.nextPageLabel = "Siguiente";
+          this.dataSource.paginator._intl.previousPageLabel = "Anterior";
+          this.dataSource.paginator._intl.firstPageLabel = "Inicio";
+          this.dataSource.paginator._intl.lastPageLabel = "Última página";
           this.ocultarMensaje = true;
           this.spinnerService.hide();
         }
