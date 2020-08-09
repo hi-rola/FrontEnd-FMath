43a88353826984354cbe4ef8fd36d9e2f0e73564
hi-rola <rolaalbaa@gmail.com>
2020-08-08 19:13:06 -0500

    Se modifica para acceso administrador.

--- src/app/components/mensajes-de-confirmacion/msj-cancelar-registro/msj-cancelar-registro.component.ts
+++ src/app/components/mensajes-de-confirmacion/msj-cancelar-registro/msj-cancelar-registro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatDialogRef } from '@angular/material';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-msj-cancelar-registro',
@@ -9,16 +10,28 @@ import { MatDialogRef } from '@angular/material';
 })
 export class MsjCancelarRegistroComponent implements OnInit {
 
-  constructor(private router: Router, public dialogRef: MatDialogRef<MsjCancelarRegistroComponent>) { }
+  constructor(private router: Router, public dialogRef: MatDialogRef<MsjCancelarRegistroComponent>,
+    private loginService: LoginService) { }
 
   ngOnInit() { }
 
   cancelarSi() {
-    this.router.navigate(['/home-administrador']);
-    this.dialogRef.close();
+    const tipoUsuario = this.getTipoUsuario();
+    if (tipoUsuario === 'a') {
+      this.router.navigate(['/home-academico']);
+      this.dialogRef.close();
+    } else if (tipoUsuario === 'd') {
+      this.router.navigate(['/home-administrador']);
+      this.dialogRef.close();
+    }
   }
 
   cancelarNo() {
     this.dialogRef.close();
+  }
+
+  getTipoUsuario(): string {
+    const tipoUsuario = this.loginService.getTipoUsuario();
+    return tipoUsuario;
   }
 }
